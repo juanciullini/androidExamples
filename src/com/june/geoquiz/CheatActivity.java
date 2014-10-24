eed1d6ed078ab4b00ddb43f33e586db7d5757d97
Juan Ciullini <juanciullini@gmail.com>
2014-10-24 17:39:51 -0300

    cheat activity chapter 5

---
+++ src/com/june/geoquiz/CheatActivity.java
@@ -0,0 +1,14 @@
+package com.june.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheatActivity extends Activity {
+	
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cheat);
+	}
+
+}
